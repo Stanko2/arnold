ff3f32faf1fc3e481c8fd334c8b47aa09ac6e9e7
jaksia <35041897+jaksia@users.noreply.github.com>
2022-05-11 18:59:11 +0200

    Added manual transparency input
    Fixed a miss-phrased word
    Converted absolute value to percentage

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <b-button
-      :id="name"
-      class="colorInputButton"
-      :style="{
+        :id="name"
+        class="colorInputButton"
+        :style="{
         'background-color': color,
       }"
     ></b-button>
@@ -11,15 +11,25 @@
       <template #title>{{ label }}</template>
       <v-swatches v-model="color" inline @input="submit"></v-swatches>
       <div class="d-flex align-items-center justify-content-between">
-        <p class="d-flex align-items-center">Priehľadnosť</p>
+        <p class="d-flex align-items-center transparency-text">Nepriehľadnosť</p>
+        <!-- percentage input -->
+        <div class="form-control transparency-num">
+          <input
+              type="number"
+              class="transparency-num"
+              min="6"
+              max="100"
+              v-model.number="transparency"
+              @input="submit"
+          /></div>
         <b-form-input
-          min="30"
-          style="width: 100px"
-          type="range"
-          max="255"
-          step="1"
-          v-model.number="transparency"
-          @input="submit"
+            min="7"
+            style="width: 80px"
+            type="range"
+            max="100"
+            step="1"
+            v-model.number="transparency"
+            @input="submit"
         />
       </div>
     </b-popover>
@@ -58,11 +68,11 @@ export default Vue.extend({
   },
   mounted() {
     this.color = this.value.substring(0, 7);
-    this.transparency = parseInt(this.value.substring(7, 9), 16) || 255;
+    this.transparency = Math.round((parseInt(this.value.substring(7, 9), 16)/255)*100) || 100;
   },
   methods: {
     submit() {
-      this.$emit("input", this.color + this.transparency.toString(16));
+      this.$emit("input", this.color + Math.floor((this.transparency*255)/100).toString(16));
     },
   },
 });
@@ -75,4 +85,48 @@ export default Vue.extend({
   border-radius: 10px;
   border: none;
 }
+
+p.transparency-text {
+  margin-right: 10px;
+}
+
+div.transparency-num {
+  width: 75px;
+  position: relative;
+  padding-top: 4px;
+  padding-bottom: 4px;
+  padding-left: 4px;
+  margin-right: 10px;
+}
+
+div.transparency-num::after {
+  content: "%";
+  display: inline;
+  position: absolute;
+  right: 5px;
+  top: 5px;
+}
+
+input.transparency-num::-webkit-outer-spin-button,
+input.transparency-num::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+input[type=number].transparency-num {
+  -moz-appearance: textfield;
+}
+input.transparency-num {
+  width: 30px;
+  margin-right: 10px;
+  display: inline;
+}
+
+input.transparency-num,
+input.transparency-num:focus,
+input.transparency-num:active {
+  border: none;
+}
+
+
 </style>
