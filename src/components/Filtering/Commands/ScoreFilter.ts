daacb65ad5d8e909f9cb0287eff4a0ae062b8c93
Jakub Šiagi <jakub.siagi@gmail.com>
2022-11-05 22:35:31 +0100

    ScoreFilter now uses closed interval

--- src/components/Filtering/Commands/ScoreFilter.ts
+++ src/components/Filtering/Commands/ScoreFilter.ts
@@ -1,17 +1,17 @@
 import {FilterCommand} from '../Filter';
-import { Document } from '@/@types';
+import {Document} from '@/@types';
 
 export class ScoreFilter implements FilterCommand {
     matches(doc: Document): boolean {
         if(this.args.length == 0)
             return doc.scoring?.points != undefined;
         if(!doc.scoring) return false;
-        return doc.scoring.points > parseFloat(this.args[0]) && doc.scoring.points < parseFloat(this.args[1]);
+        return doc.scoring.points >= parseFloat(this.args[0]) && doc.scoring.points <= parseFloat(this.args[1]);
     }
     name = 'body';
     args = [];
     inversed = false;
-    
+
     valid(): boolean {
         return this.args.length == 0 || (this.args.length == 2 && !isNaN(this.args[0]) && !isNaN(this.args[1]));
     }
