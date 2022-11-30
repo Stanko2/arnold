222a7a5a0282370914eccb7d4bab31892cdaf5e9
Jakub Šiagi <jakub.siagi@gmail.com>
2022-11-30 22:57:52 +0100

    First version of auto update

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import {BootstrapVue, BvToastOptions, IconsPlugin} from 'bootstrap-vue'
 import App from './router/RouterMain.vue'
 
 import "bootstrap-vue/dist/bootstrap-vue.css"
@@ -10,6 +10,27 @@ import './registerServiceWorker'
 import './Mixins/EventHub'
 import store from './Store'
 
+let loaded = false
+let toast: { message: string, options: BvToastOptions }[] = []
+let modal: { type: string, title: string, message: string, options: BvToastOptions, callback: () => void }[] = []
+
+export const showToast = (message: string, options: BvToastOptions) => {
+    if (loaded) {
+        app.$bvToast.toast(message, options)
+    } else {
+        toast.push({message, options})
+    }
+}
+
+export const showModal = (type: string, title: string, message: string, options: BvToastOptions, callback: () => void) => {
+    if (loaded) {
+        // @ts-ignore
+        app.$bvModal["msgBox" + type]?.(message, options).then(callback)
+    } else {
+        modal.push({type, title, message, options, callback})
+    }
+}
+
 const shortkey = require('vue-shortkey');
 const ctxMenu = require('vue-context-menu');
 
@@ -19,8 +40,16 @@ Vue.use(IconsPlugin)
 Vue.use(shortkey, { prevent: ['input', 'textarea'] })
 // Vue.use(ctxMenu)
 
-new Vue({
+const app = new Vue({
   router,
   store,
-  render: h => h(App),
+    mounted() {
+        loaded = true
+        toast.forEach(t => app.$bvToast.toast(t.message, t.options))
+        modal.forEach(m => {
+            // @ts-ignore
+            app.$bvModal["msgBox" + m.type]?.(m.message, m.options).then(m.callback)
+        })
+    },
+  render: h => h(App)
 }).$mount('#app')
--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 
-import { register } from 'register-service-worker'
+import {register} from 'register-service-worker'
+import {showModal, showToast} from "@/main";
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
@@ -15,18 +16,44 @@ if (process.env.NODE_ENV === 'production') {
     },
     cached () {
       console.log('Content has been cached for offline use.')
+
+      showToast('Pripravené na používanie bez internetu', {
+        title: 'Status',
+        variant: 'success',
+        solid: true
+        })
     },
     updatefound () {
       console.log('New content is downloading.')
     },
-    updated () {
+    updated (registration) {
       console.log('New content is available; please refresh.')
+
+      showModal('Confirm', 'Aktualizácia', 'Je dostupná nová verzia aplikácie. Chcete ju teraz nainštalovať?', {
+        title: 'Aktualizácia',
+        variant: 'success',
+        solid: true
+        }, () => {
+          navigator.serviceWorker.addEventListener('controllerchange', () => {
+            window.location.reload();
+          });
+
+          if (!registration || !registration.waiting) return;
+          registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+        })
+
     },
     offline () {
       console.log('No internet connection found. App is running in offline mode.')
     },
     error (error) {
       console.error('Error during service worker registration:', error)
+
+      showToast('Nepodarilo sa nainštalovať Service Worker. Ak sa toto opakuje, napíšte nám.', {
+        title: 'Status',
+        variant: 'danger',
+        solid: true
+      })
     }
   })
 }
