2f80ce2bea042ba7eca0737a7aa7b65b11142a97
Jakub Šiagi <jakub.siagi@gmail.com>
2023-02-26 11:38:09 +0100

    Fix rectangles and downgrade fabric version

--- src/Annotation/RectAnnotation.ts
+++ src/Annotation/RectAnnotation.ts
@@ -1,8 +1,8 @@
-import { Canvas } from "@/Canvas";
-import { Annotation } from "./Annotation";
+import {Canvas} from "@/Canvas";
+import {Annotation} from "./Annotation";
 import Color from "color";
-import { fabric } from "fabric";
-import { degrees, PDFPage, popGraphicsState, pushGraphicsState, rgb, rotateDegrees, translate } from "pdf-lib";
+import {fabric} from "fabric";
+import {PDFPage, popGraphicsState, pushGraphicsState, rgb, rotateDegrees, translate} from "pdf-lib";
 
 export class RectAnnotation extends Annotation {
     static toolOptions: any;
@@ -20,6 +20,7 @@ export class RectAnnotation extends Annotation {
         const center = new fabric.Point((this.object.width || 0) / 2, (this.object.height || 0) / 2);
         let pos = new fabric.Point((this.object.left || 0), height - (this.object.top || 0));
         page.pushOperators(
+            pushGraphicsState(),
             translate(pos.x, pos.y),
             rotateDegrees(-(this.object.angle || 0)),
             translate(-pos.x, -pos.y)
@@ -36,6 +37,7 @@ export class RectAnnotation extends Annotation {
             opacity: parseInt((this.object.fill as string).substring(7, 9), 16) / 255 || 1,
             borderOpacity: parseInt((this.object.stroke as string).substring(7, 9), 16) / 255 || 1,
         });
+        page.pushOperators(popGraphicsState());
     }
     serialize(): any {
         return {
